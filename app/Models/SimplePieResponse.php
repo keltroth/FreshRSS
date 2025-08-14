531b9286d94f2f38e8725284bc2bd038f32d743e
Alexandre Alapetite <alexandre@alapetite.fr>
2025-08-14 09:35:30 +0200

    Fix regression on_http_response (#7824)
    Fix regression (bad merge) from https://github.com/FreshRSS/FreshRSS/pull/7775
    * https://github.com/FreshRSS/simplepie/pull/50

--- app/Models/SimplePieResponse.php
+++ app/Models/SimplePieResponse.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 final class FreshRSS_SimplePieResponse extends \SimplePie\File
 {
 	#[\Override]
-	protected function on_http_response(string|false $response = ''): void {
+	protected function on_http_response($response = ''): void {
 		syslog(LOG_INFO, 'FreshRSS SimplePie GET ' . $this->get_status_code() . ' ' . \SimplePie\Misc::url_remove_credentials($this->get_final_requested_uri()));
 
 		if (in_array($this->get_status_code(), [429, 503], true)) {
